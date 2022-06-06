57d176c0596533fee0c31a7f06d2f0cdadfd06c0
Gaojun5100 <304010540@qq.com>
2022-06-06 23:15:17 +0800

    first

---
+++ src/container/Home/component/Courses/index.jsx
@@ -0,0 +1,59 @@
+import styles from './style.module.scss'
+const  Banner = () => {
+  return (
+    <div className={styles.courses}>
+      <ul className={styles.list}>
+        <li className={styles.item}>
+          <img className={styles.img} src='https://s0.lgstatic.com/i/image6/M00/41/5A/Cgp9HWCrqJeAfHpMAAEhcFmUStA456.png' alt=''/>
+          <h4 className={styles.title}>Vue.js 框架源码与进阶</h4>
+          <p className={styles.desc}>围绕当下国内最主流的前端核心框架 Vue.js 展开，深入框架内部，通过解读源码或者
+          手写实现的方式，剖析 Vue.js 框架的内部实现原理，让你做到知其所以然。同时我们还会介绍 Vue.js 的进阶用法、周
+          边生态以及性能优化，让你轻松应对更加复杂的项目业务需求。
+          </p>
+        </li>
+        <li className={styles.item}>
+          <img className={styles.img} src='https://s0.lgstatic.com/i/image6/M00/41/5A/Cgp9HWCrqJeAfHpMAAEhcFmUStA456.png' alt=''/>
+          <h4 className={styles.title}>Vue.js 框架源码与进阶</h4>
+          <p className={styles.desc}>围绕当下国内最主流的前端核心框架 Vue.js 展开，深入框架内部，通过解读源码或者
+          手写实现的方式，剖析 Vue.js 框架的内部实现原理，让你做到知其所以然。同时我们还会介绍 Vue.js 的进阶用法、周
+          边生态以及性能优化，让你轻松应对更加复杂的项目业务需求。
+          </p>
+        </li>
+        <li className={styles.item}>
+          <img className={styles.img} src='https://s0.lgstatic.com/i/image6/M00/41/5A/Cgp9HWCrqJeAfHpMAAEhcFmUStA456.png' alt=''/>
+          <h4 className={styles.title}>Vue.js 框架源码与进阶</h4>
+          <p className={styles.desc}>围绕当下国内最主流的前端核心框架 Vue.js 展开，深入框架内部，通过解读源码或者
+          手写实现的方式，剖析 Vue.js 框架的内部实现原理，让你做到知其所以然。同时我们还会介绍 Vue.js 的进阶用法、周
+          边生态以及性能优化，让你轻松应对更加复杂的项目业务需求。
+          </p>
+        </li>
+        <li className={styles.item}>
+          <img className={styles.img} src='https://s0.lgstatic.com/i/image6/M00/41/5A/Cgp9HWCrqJeAfHpMAAEhcFmUStA456.png' alt=''/>
+          <h4 className={styles.title}>Vue.js 框架源码与进阶</h4>
+          <p className={styles.desc}>围绕当下国内最主流的前端核心框架 Vue.js 展开，深入框架内部，通过解读源码或者
+          手写实现的方式，剖析 Vue.js 框架的内部实现原理，让你做到知其所以然。同时我们还会介绍 Vue.js 的进阶用法、周
+          边生态以及性能优化，让你轻松应对更加复杂的项目业务需求。
+          </p>
+        </li>
+        <li className={styles.item}>
+          <img className={styles.img} src='https://s0.lgstatic.com/i/image6/M00/41/5A/Cgp9HWCrqJeAfHpMAAEhcFmUStA456.png' alt=''/>
+          <h4 className={styles.title}>Vue.js 框架源码与进阶</h4>
+          <p className={styles.desc}>围绕当下国内最主流的前端核心框架 Vue.js 展开，深入框架内部，通过解读源码或者
+          手写实现的方式，剖析 Vue.js 框架的内部实现原理，让你做到知其所以然。同时我们还会介绍 Vue.js 的进阶用法、周
+          边生态以及性能优化，让你轻松应对更加复杂的项目业务需求。
+          </p>
+        </li>
+        <li className={styles.item}>
+          <img className={styles.img} src='https://s0.lgstatic.com/i/image6/M00/41/5A/Cgp9HWCrqJeAfHpMAAEhcFmUStA456.png' alt=''/>
+          <h4 className={styles.title}>Vue.js 框架源码与进阶</h4>
+          <p className={styles.desc}>围绕当下国内最主流的前端核心框架 Vue.js 展开，深入框架内部，通过解读源码或者
+          手写实现的方式，剖析 Vue.js 框架的内部实现原理，让你做到知其所以然。同时我们还会介绍 Vue.js 的进阶用法、周
+          边生态以及性能优化，让你轻松应对更加复杂的项目业务需求。
+          </p>
+        </li>
+      </ul>      
+    </div>
+  );
+}
+
+export default Banner;
